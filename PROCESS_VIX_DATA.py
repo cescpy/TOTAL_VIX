fbf2078c224e57e9ae12634fc103293db11aa915
Cesc_py <cescmateu@hotmail.com>
2023-06-20 22:32:49 +0200

    update

--- PROCESS_VIX_DATA.py
+++ PROCESS_VIX_DATA.py
@@ -8,9 +8,9 @@ def combine_dictDB(dicts = []):
     AJUNTA LES DOS BD DE FUTURS I INDEXS EN UN SOL DICCIONARI
     '''
     
-    data_combined = dicts[0]
-    for i in dicts[:-1]:
-        data_combined = data_combined.update(dicts[i+1])
+    data_combined = dicts[0].copy()
+    for i, dic in enumerate(dicts[:-1]):
+        data_combined.update(dicts[i+1])
      
     return data_combined
 
